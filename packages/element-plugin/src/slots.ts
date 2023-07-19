ea7d986b4ff65d0b83bc93181acc07c9e721f4b9
aring <1873748463@qq.com>
2023-07-19 08:17:05 +0800

    fix: jsx

--- packages/element-plugin/src/slots.ts
+++ packages/element-plugin/src/slots.ts
@@ -1,16 +1,12 @@
 import { ComponentType, Reactive, SceneContext, Scheme } from '@koala-form/core';
 import { ElButton, ElCheckbox, ElRadio, ElSpace } from 'element-plus';
-import { VNode, unref } from 'vue';
+import { unref, h } from 'vue';
 
 const optComps = { [ComponentType.CheckboxGroup]: ElCheckbox, [ComponentType.RadioGroup]: ElRadio };
 export const genOptions = (name: string, props?: Reactive) => {
     const Comp = optComps[name];
     return () => {
-        return unref(unref(props || {}).options)?.map((item: any) => (
-            <Comp {...item} label={item.value}>
-                {item.label}
-            </Comp>
-        ));
+        return unref(unref(props || {}).options)?.map((item: any) => h(Comp, { ...item, label: item.value }, () => item.label));
     };
 };
 
@@ -32,12 +28,8 @@ export const genModalFooter = (scheme: Scheme, ctx: SceneContext) => {
     };
 
     return () =>
-        (
-            <ElSpace>
-                <ElButton onClick={onCancel}>{unref(scheme.props || {}).cancelText || '取消'}</ElButton>
-                <ElButton onClick={onOk} type="primary">
-                    {unref(scheme.props || {}).okText || '确定'}
-                </ElButton>
-            </ElSpace>
-        ) as unknown as VNode[];
+        h(ElSpace, {}, () => [
+            h(ElButton, { onClick: onCancel }, () => unref(scheme.props || {}).cancelText || '取消'),
+            h(ElButton, { onClick: onOk, type: 'primary' }, () => unref(scheme.props || {}).okText || '确定'),
+        ]);
 };
